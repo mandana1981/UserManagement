a7001c88e6014b2c709f0d938df53355dbf02b24
m.abbasifarvad <0016439597@Maf>
2024-10-25 22:26:52 +0330

    Add swagger Comment

--- src/main/java/ca/sematec/springproject/controller/UserController.java
+++ src/main/java/ca/sematec/springproject/controller/UserController.java
@@ -3,8 +3,14 @@ package ca.sematec.springproject.controller;
 import ca.sematec.springproject.dto.UserDTO;
 import ca.sematec.springproject.entity.User;
 import ca.sematec.springproject.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,6 +23,13 @@ public class UserController {
     @Autowired
     UserService userService;
 
+
+    @Operation(summary = "Return List of all users ")
+    @ApiResponses(value = {
+
+            @ApiResponse(responseCode = "200", description = "OK!",
+                    content = {@Content(mediaType = "application/json"
+                            , schema = @Schema(implementation = List.class))})})
     @GetMapping
     public ResponseEntity<List<User>> getAllUsers() {
         List<User> users = userService.getAllUsers();
@@ -31,9 +44,20 @@ public class UserController {
         return ResponseEntity.ok(userDTO);
     }
 
+
+
+    @Operation(summary = "Create new User")
+    @ApiResponses(value = {
+
+            @ApiResponse(responseCode = "201", description = "Created !",
+                    content = {@Content(mediaType = "application/json"
+                            , schema = @Schema(implementation = User.class))}),
+            @ApiResponse(responseCode = "400", description = "Invalid input")
+            , @ApiResponse(responseCode = "409", description = "This user already exists")})
     @PostMapping
     public ResponseEntity<User> addUser(@RequestBody User user) {
-        return ResponseEntity.ok(userService.addUser(user));
+        return  new ResponseEntity<User>(userService.addUser(user), HttpStatus.CREATED);
+
     }
 
 //    @DeleteMapping("/{id}")

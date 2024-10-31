c75d2bc0bbe50d7e6a4e499536a34284c38e56c6
m.abbasifarvad <0016439597@Maf>
2024-10-31 21:43:40 +0330

    Code review and add swagger comment ad api interface

--- src/main/java/ca/sematec/springproject/api/UserAPI.java
+++ src/main/java/ca/sematec/springproject/api/UserAPI.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 
+
 @Tag(name = "USERS", description = "User Api")
 @RequestMapping("/users")
 public interface UserAPI {
